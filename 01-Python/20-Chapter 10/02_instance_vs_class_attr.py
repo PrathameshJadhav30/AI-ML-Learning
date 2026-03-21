ca8b76184a3ddc9c8481788d63ac06299d8cef1f
Prathamesh <prathameshaj2004@gmail.com>
2026-03-21 20:52:18 +0530

    Python

---
+++ 01-Python/20-Chapter 10/02_instance_vs_class_attr.py
@@ -0,0 +1,9 @@
+class Employee: 
+    language = "Python" # This is a class attribute
+    salary = 1200000
+
+
+Prathamesh = Employee()
+Prathamesh.language = "JavaScript" # This is an instance attribute
+print(Prathamesh.language, Prathamesh.salary)
+ 
